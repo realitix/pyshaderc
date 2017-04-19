86b2a37518c9918d86f2f274c59680d267b4fbb8
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-04-19 15:54:05 +0200

    Update windows compilation options

--- _cffi_build/pyshaderc_build.py
+++ _cffi_build/pyshaderc_build.py
@@ -49,10 +49,10 @@ def build():
 
     if platform.system() == 'Windows':
         options += [
-            '-DCMAKE_C_FLAGS=" /nologo /EHSC"',
-            '-DCMAKE_CXX_FLAGS=" /nologo /EHSC"',
-            '-DCMAKE_C_FLAGS_RELEASE=" /nologo /EHSC"',
-            '-DCMAKE_CXX_FLAGS_RELEASE=" /nologo /EHSC"'
+            '-DCMAKE_C_FLAGS=" /nologo /EHsc"',
+            '-DCMAKE_CXX_FLAGS=" /nologo /EHsc"',
+            '-DCMAKE_C_FLAGS_RELEASE=" /nologo /EHsc"',
+            '-DCMAKE_CXX_FLAGS_RELEASE=" /nologo /EHsc"'
         ]
 
     call = ['cmake', '-B'+SHADERC_BIN, '-H'+SHADERC_SRC]
